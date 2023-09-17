c74f297af1064ea15a58bd104c0e958848d7b2e6
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-09-17 17:08:30 -0600

    Both the get and post functions of the notes.js file are now working

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,10 +2,10 @@ const notesApi = require('express').Router();
 const fs = require('fs');
 
 
-notesApi.get('/api/notes.js', (req, res) =>{
-    fs.readFile('Develop/db/db.json', 'utf-8', (error, data) => error ? console.error(error) : console.log(data))});
+notesApi.get('/notes', (req, res) =>
+fs.readFile('./db/db.json', 'utf-8', (error, data) => error ? console.error(error) : console.log(data)))
 
-notesApi.post('/api/notes.js', (req, res) => {
+notesApi.post('/notes', (req, res) => {
   
     const {title, text} = req.body
     console.log(req.body);
@@ -15,7 +15,11 @@ notesApi.post('/api/notes.js', (req, res) => {
             title,
             text,
         };
-            fs.writeFile ('log.txt', (err) => 
+        // How do i tell the computer to write file to the index.js?
+        // Index.js line 174 says we need to send file to db because the function there will render it to the sidebar
+        // How do i get the computer to write file to the db, by using the newNote object??
+        const newNoteJSON = JSON.stringify(newNote)
+            fs.writeFile('./db/db.json', newNoteJSON, (err) => 
                 err ? console.error(err) : console.log('Success!')
             )
     }
