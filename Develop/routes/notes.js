06e403075cc61236cf1ea89b94c55a72959163af
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-09-17 18:22:20 -0600

    Added returned data to readfile function

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 
 notesApi.get('/notes', (req, res) =>
-fs.readFile('./db/db.json', 'utf-8', (error, data) => error ? console.error(error) : console.log(data)))
+fs.readFile('./db/db.json', 'utf-8', (error, data) => error ? console.error(error) : res.json(JSON.parse(data))));
 
 notesApi.post('/notes', (req, res) => {
   
@@ -15,8 +15,9 @@ notesApi.post('/notes', (req, res) => {
             title,
             text,
         };
+        
         const newNoteJSON = JSON.stringify(newNote)
-
+        
             fs.writeFile('./db/db.json', newNoteJSON, (err) => 
                 err ? console.error(err) : console.log('Success!')
             )
