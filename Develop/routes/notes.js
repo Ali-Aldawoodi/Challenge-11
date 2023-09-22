6e0d9d2c56694f7a596fd23dc009a80638b3e70b
Ali Aldawoodi <124013342+Ali-Aldawoodi@users.noreply.github.com>
2023-09-22 12:28:38 -0600

    Restructured entire function in order to get writefile to post correctly. Added an express middleware

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,11 +1,14 @@
-const notesApi = require('express').Router();
-const { response } = require('express');
+const express = require('express');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 
+const notesApi = express.Router();
+
+notesApi.use(express.json());
+
 notesApi.get('/notes', (req, res) =>
-    fs.readFile('./db/db.json', 'utf-8', (error, data) => error ? console.error(error) : res.json([JSON.parse(data)])));
+    fs.readFile('./db/db.json', 'utf-8', (error, data) => error ? console.error(error) : res.json(JSON.parse(data))));
 
 
 
@@ -15,39 +18,47 @@ notesApi.post('/notes', (req, res) => {
     console.log(req.body);
 
     if (title && text) {
-        const newNote = {
-            title,
-            text,
-            newNoteID: uuidv4(),
-        };
 
         fs.readFile('./db/db.json', 'utf-8', (err, data) => {
 
             if (err) {
                 console.error(err);
                 res.status(500).send('Server Error');
-                return;
             } else {
-            const parsedNotesArr = [JSON.parse(data)]; // This worked because this has to be an array for json to read it??
+                const parsedNotesArr = JSON.parse(data); // This worked because this has to be an array for json to read it??
+                const newNote = {
+                    title,
+                    text,
+                    newNoteID: uuidv4(),
+                };
 
-            parsedNotesArr.push(newNote);
+                parsedNotesArr.push(newNote);
 
-            const updatedNotes = JSON.stringify(parsedNotesArr);
+                // const updatedNotes = JSON.stringify(parsedNotesArr);
 
+                // I want to add the new note to the end of the array but when we press save i want to reset the form. 
+                // make db.json an array.
+                console.log(notesApi)
 
-            // make db.json an array.
-            console.log(notesApi)
-
-            fs.writeFile('./db/db.json', updatedNotes, (err) =>
-                err ? console.error(err) : console.log('Success!')
-            )
-            }
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotesArr), (err) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(500).send('Failed');
+                    } else {
+                        const response = {
+                            status: 'success',
+                            body: newNote,
+                        };
+                        console.log(response);
+                        res.status(201).json(response);
+                    }
+                });
+            };
         });
-
-        } else {
-    res.status(400).send('Bad Request');
+    } else {
+        res.status(400).send('Bad Request');
     }
-}); 
+});
 
 
 module.exports = notesApi;
